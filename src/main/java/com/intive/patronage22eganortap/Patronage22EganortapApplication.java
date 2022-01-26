9986a21aada6ba1176d385f6f147aaf091e0dbe8
Joanna Sokolowska <joanna_s@op.pl>
2022-01-26 22:46:40 +0100

    Add jacoco config to pom.xml

--- src/main/java/com/intive/patronage22eganortap/Patronage22EganortapApplication.java
+++ src/main/java/com/intive/patronage22eganortap/Patronage22EganortapApplication.java
@@ -1,13 +1,12 @@
 package com.intive.patronage22eganortap;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Patronage22EganortapApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Patronage22EganortapApplication.class, args);
     }
-
 }
