8ad96cca187a7bcde24fea067026872b60bf392f
Joanna Sokolowska <joanna_s@op.pl>
2022-02-28 21:52:22 +0100

    Correct date and time format

--- src/main/java/com/intive/patronage22eganortap/web/util/ActualDate.java
+++ src/main/java/com/intive/patronage22eganortap/web/util/ActualDate.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class ActualDate {
     
-    private static final DateTimeFormatter standardFormat = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
+    private static final DateTimeFormatter standardFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public static String getStandardDate() {
         return LocalDateTime.now().format(standardFormat);
