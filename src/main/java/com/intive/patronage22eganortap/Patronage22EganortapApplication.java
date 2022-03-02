90c30c2d54b54a66903d610fb2d68812c4216a51
Pawel-Koz <63590514+Pawel-Koz@users.noreply.github.com>
2022-03-02 16:17:47 +0100

    P2022-10: add Springdoc-openapi dependencies, add swagger annotations… (#14)
    * P2022-10: add Springdoc-openapi dependencies, add swagger annotations int the HelloController,
    add TeamMember in model package as an example(marked as deprecated, for removal)
    
    * P2022-10: remove commented dependencies from pom and HelloController, add version veriables for spring doc dependencies
    
    * move model package under web package
    
    * Add swagger notations to hello method, remove teamMember class and all related methods, use 1 variable for spring docs dependencies

--- src/main/java/com/intive/patronage22eganortap/Patronage22EganortapApplication.java
+++ src/main/java/com/intive/patronage22eganortap/Patronage22EganortapApplication.java
@@ -1,9 +1,11 @@
 package com.intive.patronage22eganortap;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class Patronage22EganortapApplication {
 
     public static void main(String[] args) {
