aca24fd16e1f52fa03f0e6901a46362142538a5b
sylwia <sylwia.rudawska123@gmail.com>
2022-03-15 16:37:24 +0100

    feature/P2022-1069: Add migration scripts and flyway configuration

--- src/main/resources/db/migration/dummy/V20220312194150__INSERT_DUMMY_DATA.sql
+++ src/main/resources/db/migration/dummy/V20220312194150__INSERT_DUMMY_DATA.sql
@@ -5,7 +5,7 @@ INSERT INTO question(poll_id, question_content)
 VALUES ((SELECT id FROM public.poll WHERE name = 'Zdrowe odżywianie'), 'Ile posiłków jesz dziennie?');
 
 INSERT INTO possible_answers(question_id, possible_answers_content)
-VALUES((SELECT  id FROM public.question WHERE question_content = 'Ile posiłków jesz dziennie?'), 'powyżej 5'),
+VALUES((SELECT id FROM public.question WHERE question_content = 'Ile posiłków jesz dziennie?'), 'powyżej 5'),
       ((SELECT id FROM public.question WHERE question_content = 'Ile posiłków jesz dziennie?'), '3 posiłki'),
       ((SELECT id FROM public.question WHERE question_content = 'Ile posiłków jesz dziennie?'), 'poniżej 3');
 
